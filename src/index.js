e875a955a36df312258424dc1f0b5cd8fd086890
Rebecca Huseyin <rebecca.huseyin.14@ucl.ac.uk>
2019-05-14 15:46:15 +0100

    Add to do list functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const tasksEl = document.getElementById("tasks")
+  const formEl = document.getElementById("create-task-form")
+  const input = document.getElementById("new-task-description")
+
+  function addTask (task) {
+    const li = document.createElement('li')
+
+    li.innerHTML = "task <button class='delete-btn'>X</button>"
+
+    tasksEl.append(li)
+
+    const deleteBtn = li.querySelector('.delete-btn')
+    deleteBtn.addEventListener('click', function (event) {
+      li.remove()
+    })
+  }
+
+  formEl.addEventListener('submit', function(event) {
+    event.preventDefault()
+  
+    let task = input.value
+
+    addTask(task)
+  })
+
 });
+
+
